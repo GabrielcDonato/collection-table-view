1838516645596ab2d4039f5f9713446af42e1095
Gabriel Donato <premiersoft-gabriel.donato@ailos.coop.br>
2023-09-11 21:52:42 -0300

    Initial Commit

---
+++ collect_table_view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collect_table_view
+//
+//  Created by Premiersoft on 11/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
